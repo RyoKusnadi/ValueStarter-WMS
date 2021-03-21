1a33ddca68dd54f716879ca4a5746fe5a6625725
ryokusnadi <ryokusnadi@gmail.com>
2021-03-21 10:46:41 +0700

    Fix Import Bug

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\ PermissionsMixin:
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
+
 
 class UserManager(BaseUserManager):
 
